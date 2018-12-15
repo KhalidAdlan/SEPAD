1f7c09428dc826a42a5b3ccd1bf9b60290c991b9
khalid.h <khalid.h@sybertechnology.com>
2018-12-15 13:59:29 +0200

    First deployment to heroku

--- config/settings.php
+++ config/settings.php
@@ -2,10 +2,10 @@
 
 return [
     'analytics_id'     => null,
-    'logo'             => asset('/img/logo.jpg'),
+    'logo'             => 'i/icons/logo.png',
     'login_image'      => 'https://ssl.gstatic.com/accounts/ui/avatar_2x.png',
     'site_facebook'    => 'https://www.facebook.com/',
-    'site_description' => 'SEPAD | SUDAN ELECTRONIC PAD',
+    'site_description' => 'SEPAD | SUDAN ELECTRONIC',
     'site_title'       => 'SEPAD',
     'site_twitter'     => 'https://twitter.com/',
 ];
